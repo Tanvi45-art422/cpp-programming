79e9f993fe652e7a8de646efb6e79fe4848d028c
Tanvi45-art422 <tskarande1985@gmail.com>
2026-08-05 09:02:47 +0000

    Add Areao_of_rectangle program

---
+++ Reverse_numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+int n, rem, num=0,temp;
+cout<<"Enter a number: " ;
+cin>>n;
+while(n =0)
+{ 
+rem =n%10;
+num= num*10+ rem;
+n=n/10;
+}
+if(temp==num)
+}
